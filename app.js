0a26d23390f8ad4dd70b7b5367960bcc9f51f075
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-07-03 00:14:00 +0100

    update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const timeleft = document.querySelector('#time-left')
 const score = document.querySelector('#score')
 
 let result = 0
+let hitPosition
 
 function randomSquare(){
     squares.forEach(square => {
@@ -12,6 +13,22 @@ function randomSquare(){
     
     let randomSquare = squares[Math.floor(Math.random() *  9)]
     randomSquare.classList.add('mole')
+    hitPosition = randomSquare.id;
 }
 
-randomSquare()
+squares.forEach(square => {
+    square.addEventListener('mousedown' ,() =>{
+        if(square.id == hitPosition){
+            result++
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+})
+
+function moveMole(){
+    let timerId = null
+    timerId = setInterval(randomSquare, 500)
+}
+
+moveMole()
